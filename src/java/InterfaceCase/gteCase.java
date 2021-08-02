63b3620281af53e3b2ef63dc56fffc7a783af1da
214615 <zjp214615@163.com>
2021-08-02 15:22:03 +0800

    接口自动化修改

--- src/java/InterfaceCase/gteCase.java
+++ src/java/InterfaceCase/gteCase.java
@@ -1,5 +1,7 @@
 package InterfaceCase;
 
+import org.testng.annotations.Test;
+
 import javax.print.DocFlavor;
 import java.io.*;
 import java.net.URL;
@@ -7,9 +9,11 @@ import java.net.URLConnection;
 import java.util.List;
 import java.util.Map;
 
-public class GteCase {
+
+public class gteCase {
+    @Test
     public static String sendget(String url,String param) throws Exception{
-        String result="";
+        String reaurl="";
         BufferedReader in=null;
         try{
             String urlNameString=url+"?"+param;
@@ -34,7 +38,7 @@ public class GteCase {
             String line;
             //readLine()读取文件内容
             while ((line=in.readLine())!=null){
-                result+=line;
+                reaurl +="\b"+line;
             }
 
         }catch (Exception e){
@@ -43,42 +47,7 @@ public class GteCase {
                 if(in!=null)
                 in.close();
         }
-        return result;
-    }
-    public static String sendPost(String url,String param) throws Exception {
-        PrintWriter out=null;
-        BufferedReader in=null;
-        String result="";
-        try{
-            URL reaUrl=new URL(url);
-            URLConnection connection= reaUrl.openConnection();
-            connection.setRequestProperty("","");
-            connection.setRequestProperty("","");
-            connection.setRequestProperty("","");
-            //发送Post请求必须设置
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            // 获取URLConnection对象对应的输出流
-            out =new PrintWriter(connection.getOutputStream());
-            // 发送请求参数
-            out.print(param);
-            //输出流的缓冲
-            out.flush();
-            // 定义BufferedReader输入流来读取URL的响应
-            in=new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line;
-            while ((line=in.readLine())!=null){
-                result +=line;
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            if (in != null)
-                in.close();
-            if (out != null)
-                out.close();
-        }
-        return result;
+        return reaurl;
     }
 }
 
--- src/java/InterfaceCase/postCase.java
+++ src/java/InterfaceCase/postCase.java
@@ -1,4 +1,47 @@
 package InterfaceCase;
 
-public class PostCase {
+import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+public class postCase {
+    @Test
+    public static void senfPost(String url,String param) throws Exception{
+        PrintWriter out=null;
+        BufferedReader in=null;
+        try{
+            URL reaurl=new URL(url);
+            URLConnection connection= reaurl.openConnection();
+            //设置通用请求参数
+            connection.setRequestProperty("","");
+            connection.setRequestProperty("","");
+            connection.setRequestProperty("","");
+            //发送Post请求必须设置如下两行
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            out=new PrintWriter(connection.getOutputStream());
+            //发送请求参数
+            out.print(param);
+            //flush输出流缓冲
+            out.flush();
+            // 定义BufferedReader输入流来读取URL的响应
+            in=new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line=in.readLine())!=null){
+                System.out.println(line);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (out!=null)
+                out.close();
+            if (in!=null)
+                in.close();
+        }
+    }
 }
